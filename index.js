23c4974e40a099e5aa4f5da730eb2db7c626e76d
Matthew Rupp <mattrupp713@gmail.com>
2020-01-29 13:49:27 -0600

    Add example of const and let.

---
+++ index.js
@@ -0,0 +1,15 @@
+// PRIMITIVE data
+
+//we need a way to reuse or access data, so we create a REFERENCE
+//'const' alerts JS to reserve some space in memory
+//we provide a name for this space in memory
+//we use the ASSIGNMENT OPERATOR '='
+//this takes the VALUE on the right and assigns it to the NAMED REFERENCE
+const myString = "string";
+//const cannot have the value reassigned
+
+//use 'let' ONLY if we plan to RE-ASSIGN the VALUE REFERENCED by the VARIABLE.
+let myNum = 23;
+
+//if we're not sure what to use, use CONST for now and see if you get an error later on.
+// 'CTRL' + '/' comments out a line
