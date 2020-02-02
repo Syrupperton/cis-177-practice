75f42f3203d71037b03ebaf228388f855440aac6
Matthew Rupp <mattrupp713@gmail.com>
2020-02-02 12:46:37 -0600

    Update index file for homework assignment

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ let myNum = 23;
 
 //if we're not sure what to use, use CONST for now and see if you get an error later on.
 // 'CTRL' + '/' comments out a line
+
+//adding to what we did in class for homework
+const gameTime = "sixty minutes";
+let throwingYards = 100;
+let rushingYards = 100;
+var WIN = true;
+
+//the const is the game time which will always be sixty minutes in every football game
+//the lets are different stats could be different values in each game
+//obviously there is a lot that could be added to this but, this is a very basic start for football stats.
+// the var variables are for a win or loss depending on if the win variable is true or false
