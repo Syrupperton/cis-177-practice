bb85029a9ac1deba18fd63c076936252ec2d632d
Matthew Rupp <mattrupp713@gmail.com>
2020-02-05 13:26:22 -0600

    Add examples of NULL and UNDEFINED

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const timeOfDay = "Good Evening!";
-const greeting = "Hello and ${timeOfDay}";
-console.log("${greeting}");
+//UNDEFINED is a PRIMITIVE TYPE
+//It signifies the absence of ANY value
+let x;
 
-let newVariable;
-console.log(typeof newVariable); //undefined variable
-newVariable = "Intentionally left blank."; //string
-console.log(typeof newVariable);
-//console.log(none);  //none is not declared and will error out
-console.log("{$greeting}"); //will not run until previous line is commented out
+//NULL is a PRIMITIVE
+//NULL represents a deliberate assignment of nothing
+x = null;
+console.log(x);
 
-const name = "Matt";
-let favoriteSong = "Chemical by The Devil Wears Prada";
-let wage = 12.8;
-let age = 27;
-const onlyChild = false;
-const satisfied = true;
-let favoriteThing = "Fun";
-console.log(typeof favoriteThing);
-favoriteSong = "Wave of Youth by The Devil Wears Prada";
-favoriteThing = 13;
-age = 28;
+//This is an OBJECT
+const me = {
+  name: "Matt",
+  age: 27,
+  pets: {
+    cat: "Smokey",
+    dog: "Tux"
+  }
+};
+
+//Create a new key value pair with age = 23.
+me.age = 23;
+me.name = "Matthew";
+
+//console.log(me.pet.cat); // this would read as me.undefined.cat which will give an error because you cannot use .notation on undefined
