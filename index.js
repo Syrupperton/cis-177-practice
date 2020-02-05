05b2cdf9bfe3349bb8fc07ba88fff8cc780f6d0f
Matthew Rupp <mattrupp713@gmail.com>
2020-02-05 12:09:24 -0600

    Add Variable and Data Types Homework

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-// PRIMITIVE data
+const timeOfDay = "Good Evening!";
+const greeting = "Hello and ${timeOfDay}";
+console.log("${greeting}");
 
-//we need a way to reuse or access data, so we create a REFERENCE
-//'const' alerts JS to reserve some space in memory
-//we provide a name for this space in memory
-//we use the ASSIGNMENT OPERATOR '='
-//this takes the VALUE on the right and assigns it to the NAMED REFERENCE
-const myString = "string";
-//const cannot have the value reassigned
+let newVariable;
+console.log(typeof newVariable); //undefined variable
+newVariable = "Intentionally left blank."; //string
+console.log(typeof newVariable);
+//console.log(none);  //none is not declared and will error out
+console.log("{$greeting}"); //will not run until previous line is commented out
 
-//use 'let' ONLY if we plan to RE-ASSIGN the VALUE REFERENCED by the VARIABLE.
-let myNum = 23;
-
-//if we're not sure what to use, use CONST for now and see if you get an error later on.
-// 'CTRL' + '/' comments out a line
-
-//adding to what we did in class for homework
-const gameTime = "sixty minutes";
-let throwingYards = 100;
-let rushingYards = 100;
-var WIN = true;
-
-//the const is the game time which will always be sixty minutes in every football game
-//the lets are different stats could be different values in each game
-//obviously there is a lot that could be added to this but, this is a very basic start for football stats.
-// the var variables are for a win or loss depending on if the win variable is true or false
+const name = "Matt";
+let favoriteSong = "Chemical by The Devil Wears Prada";
+let wage = 12.8;
+let age = 27;
+const onlyChild = false;
+const satisfied = true;
+let favoriteThing = "Fun";
+console.log(typeof favoriteThing);
+favoriteSong = "Wave of Youth by The Devil Wears Prada";
+favoriteThing = 13;
+age = 28;
