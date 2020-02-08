868c8103e709b5f237b5e28fe1d6d08fcbaf671f
Matthew Rupp <mattrupp713@gmail.com>
2020-02-08 12:46:02 -0600

    Add basic player objects

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-//UNDEFINED is a PRIMITIVE TYPE
-//It signifies the absence of ANY value
-let x;
-
-//NULL is a PRIMITIVE
-//NULL represents a deliberate assignment of nothing
-x = null;
-console.log(x);
-
-//This is an OBJECT
-const me = {
-  name: "Matt",
-  age: 27,
-  pets: {
-    cat: "Smokey",
-    dog: {
-      dog1: "Tux",
-      dog2: "Stormy"
-    }
+const player = {
+  user: "Username",
+  armor: {
+    head: "helmet",
+    torso: "chestpiece",
+    arms: "gauntlets",
+    legs: "bracers"
+  },
+  weapon: {
+    melee: "sword",
+    range: "bow"
+  },
+  attributes: {
+    charisma: "50",
+    strength: "50",
+    dexterity: "50"
   }
 };
 
-//Create a new key value pair with age = 23.
-me.age = 23;
-me.name = "Matthew";
+player.armor.helmet = "Medieval";
 
-//console.log(me.pet.cat); // this would read as me.undefined.cat which will give an error because you cannot use .notation on undefined
-
-console.log(me.pets.dog.dog1);
+console.log(player.armor.helmet);
