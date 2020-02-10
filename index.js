21e1bfc89e8a21945320b88e1235f5c6d51d0f48
Matthew Rupp <mattrupp713@gmail.com>
2020-02-10 13:47:45 -0600

    Add if statement examples

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-//UNDEFINED is a PRIMITIVE TYPE
-//It signifies the absence of ANY value
-let x;
+//relational or COMPARISON OPERATORS always yields BOOLEAN.
+// console.log(1 === 2); //false
 
-//NULL is a PRIMITIVE
-//NULL represents a deliberate assignment of nothing
-x = null;
-console.log(x);
+//to check if 2 things are considered to equal...
+//'===' is STRICT EQUALITY
+//It checks the VALUE and TYPE
+// console.log(2 === "2"); //false
+// console.log(2 === 3); //false
+// console.log(2 !== 3); //not equal, true
 
-//This is an OBJECT
-const me = {
-  name: "Matt",
-  age: 27,
-  pets: {
-    cat: "Smokey",
-    dog: {
-      dog1: "Tux",
-      dog2: "Stormy"
-    }
-  }
-};
+// const x = 3;
 
-//Create a new key value pair with age = 23.
-me.age = 23;
-me.name = "Matthew";
+//EXPRESSION is anything that evaluates to a value
+//STATEMENTS combine EXPRESSIONS with keywords and other syntax
+// if (x === 3) {
+//{}'s establish SCOPE of the code
+//   console.log("x is 3!");
+// } else {
+//   console.log("x is not 3!");
+// }
 
-//console.log(me.pet.cat); // this would read as me.undefined.cat which will give an error because you cannot use .notation on undefined
+// const age = 17;
 
-console.log(me.pets.dog.dog1);
+// if (age >= 25) {
+//   console.log("Renting a car is affordable");
+// } else if (age >= 18) {
+//   console.log("Renting will be expensive!");
+// } else {
+//   console.log("You can't rent a car!");
+// }
+
+const charisma = 50;
+
+if (charisma >= 60) {
+  console.log("You barter a lower price.");
+} else if (charisma >= 40) {
+  console.log("You pay the listed price.");
+} else {
+  console.log("You cannot afford the item.");
+}
+
+const strength = 50;
+
+if (strength >= 75) {
+  console.log("You break the boulder and get the rare minerals from it.");
+} else if (strength >= 50) {
+  console.log("You break the boulder.");
+} else if (strength >= 25) {
+  console.log("The boulder cracks.");
+} else {
+  console.log("The boulder does nothing.");
+}
