2829f3e97c1eaff9d008c4002b6059fd7c271fd0
Matthew Rupp <mattrupp713@gmail.com>
2020-02-19 13:32:04 -0600

    Add Fizz Buzz solution and unary operator examples

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-//sum the numbers from 1 to 10
-let x = 0;
-let sum = 0;
-
-while (x <= 10) {
-  sum = sum + x;
-  x++;
-}
-console.log(sum);
-
 //fizz buzz
 //log the numbers from 1 to 100
 //however, if the number is divisible by 3, log "Fizz!"
@@ -17,14 +7,20 @@ console.log(sum);
 let i = 1;
 
 while (i <= 100) {
-  if (i % 3 === 0 && i % 5 === 0) {
+  if (!(i % 3) && !(i % 5)) {
     console.log(i + " FizzBuzz!");
-  } else if (i % 3 === 0) {
+  } else if (!(i % 3)) {
     console.log(i + " Fizz!");
-  } else if (i % 5 === 0) {
+  } else if (!(i % 5)) {
     console.log(i + " Buzz!");
   } else {
     console.log(i);
   }
   i++;
 }
+
+//'!' is a unary operator that auto makes the following into a boolean variable.
+console.log(Boolean(0)); //false
+console.log(!0); //true
+console.log(!"Hello"); //false
+console.log(!""); //true
