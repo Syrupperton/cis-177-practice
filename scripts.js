1392c6dd8bccfdcfc549eace0efa474d3047ed24
Matthew Rupp <mattrupp713@gmail.com>
2020-02-24 13:03:53 -0600

    Add examples for window vs global level

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello from JS!");
+console.log(window);
+
+window.console.log("Hello");
+
+//global scope alongside (NOT INSIDE) 'window'
+const me = {
+  name: "Matthew"
+};
+
+// console.log(window.me)  //undefined
+// console.log(window.me.name) //error
