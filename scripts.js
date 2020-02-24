97fa99e06506d09e4abb56a45ed2c63095483f55
Matthew Rupp <mattrupp713@gmail.com>
2020-02-24 13:46:18 -0600

    Add further examples of for loops

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
-//prompts will be good for our midterm with our current knowledge level.
-const app = {};
-
-//prompt is a BLOCKING operation. (blocks other things on the site)
-app.game = prompt("What game are you looking for?");
-if (app.game === "Destiny") {
-  app.game.gameType = prompt("What game mode are you playing?");
-}
-
-console.log(app);
-
 //log the numbers from 1 to 10.
 //in parentheses
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+//for loop scopes everything within it
+//while loop would use the global variables
+//for loop makes it much easier to track bugs
+
+//add numbers from 1 to 10
+let total = 0;
+
 for (let i = 1; i <= 10; i++) {
-  console.log(i);
+  total += i;
+
+  console.log(total);
 }
