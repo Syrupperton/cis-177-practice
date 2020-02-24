890750a77be8fe17a1b53edcbfb51956af3b69e3
Matthew Rupp <mattrupp713@gmail.com>
2020-02-24 13:32:28 -0600

    Show for loop example and prompt example

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
-console.log(window);
+//prompts will be good for our midterm with our current knowledge level.
+const app = {};
 
-window.console.log("Hello");
+//prompt is a BLOCKING operation. (blocks other things on the site)
+app.game = prompt("What game are you looking for?");
+if (app.game === "Destiny") {
+  app.game.gameType = prompt("What game mode are you playing?");
+}
 
-//global scope alongside (NOT INSIDE) 'window'
-const me = {
-  name: "Matthew"
-};
+console.log(app);
 
-// console.log(window.me)  //undefined
-// console.log(window.me.name) //error
+//log the numbers from 1 to 10.
+//in parentheses
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
