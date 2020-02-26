0b066059c54439fea887dbea03a13a651bc6f525
Matthew Rupp <mattrupp713@gmail.com>
2020-02-26 12:54:18 -0600

    Fix Homework errors for 2/26

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-let text = "";
-const r = confirm("Is that the option you wanted to select?");
-if ((r = true)) {
-  text = "Confirmed!";
-} else {
-  text = "Cancelled.";
-}
+const user = {};
+
+user.isHappy = confirm("Are you happy?");
+
+console.log(user);
