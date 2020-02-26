5c399eb7709be01e61c3ba244a6159858c9372c3
Matthew Rupp <mattrupp713@gmail.com>
2020-02-26 13:47:59 -0600

    Add examples for prompts and typeof's

--- scripts.js
+++ scripts.js
@@ -1,18 +1,10 @@
-//IMPLICIT CONVERSION
-// || means OR
-//If first EXPRESSION is 'truthy', then we don't EVALUATE the right hand side.
+let age = prompt("What is your age?");
 
-//If 'prompt' RETURNs an empty STRING - b/c of the '||' this will be IMPLICITLY converted to 'false'
-//'name' will be a STRING, but it might be blank
-let name = prompt("What is your name?");
+age = Number(age);
 
-// console.log(Number(name)) this would convert name from a 'string' to a 'number'
-
-//if 'name' is blank, then '!' will convert to a Boolean (false)
-//if 'name' is not blank, then '!' will convert to a Boolean (NOT TRUE)
-while (!name) {
-  //'while (name === "")' <-- this also would work for the 'while' line
-  name = prompt("What is your name?");
+//TODO: If the user enters something that EXPLICITLY COVERTS to 'NaN',
+//alert them.
+//if (!NaN) {
+if (!age) {
+  alert("This is not a number!");
 }
-
-console.log(name);
