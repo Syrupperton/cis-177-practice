32ae44f5a9bfe15d5cbcdc959394aeaf90a3fbc2
Matthew Rupp <mattrupp713@gmail.com>
2020-02-25 21:13:53 -0600

    Add homework for 2/26

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-//fizz buzz
-//log the numbers from 1 to 100
-//however, if the number is divisible by 3, log "Fizz!"
-//if the number is divisible by 5 , log "Buzz!"
-//if the number is divisible by 3 and 5, log "Fizzbuzz!"
+//fizz buzz exercise
 
-let i = 1;
-
-while (i <= 100) {
-  if (!(i % 3) && !(i % 5)) {
-    console.log(i + " FizzBuzz!");
-  } else if (!(i % 3)) {
-    console.log(i + " Fizz!");
-  } else if (!(i % 5)) {
-    console.log(i + " Buzz!");
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz!");
+  } else if (i % 3 === 0) {
+    console.log("Fizz!");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
   } else {
     console.log(i);
   }
-  i++;
 }
-
-//'!' is a unary operator that auto makes the following into a boolean variable.
-console.log(Boolean(0)); //false
-console.log(!0); //true
-console.log(!"Hello"); //false
-console.log(!""); //true
--- scripts.js
+++ scripts.js
@@ -1,18 +1,7 @@
-//log the numbers from 1 to 10.
-//in parentheses
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
-
-//for loop scopes everything within it
-//while loop would use the global variables
-//for loop makes it much easier to track bugs
-
-//add numbers from 1 to 10
-let total = 0;
-
-for (let i = 1; i <= 10; i++) {
-  total += i;
-
-  console.log(total);
+let text = "";
+const r = confirm("Is that the option you wanted to select?");
+if ((r = true)) {
+  text = "Confirmed!";
+} else {
+  text = "Cancelled.";
 }
