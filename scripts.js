98d9857d181363191abcba6bb4b3e4a952862848
Matthew Rupp <mattrupp713@gmail.com>
2020-02-26 13:32:08 -0600

    Add example of prompting with blank answers

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-const user = {};
+//IMPLICIT CONVERSION
+// || means OR
+//If first EXPRESSION is 'truthy', then we don't EVALUATE the right hand side.
 
-user.isHappy = confirm("Are you happy?");
+//If 'prompt' RETURNs an empty STRING - b/c of the '||' this will be IMPLICITLY converted to 'false'
+//'name' will be a STRING, but it might be blank
+let name = prompt("What is your name?");
 
-console.log(user);
+// console.log(Number(name)) this would convert name from a 'string' to a 'number'
+
+//if 'name' is blank, then '!' will convert to a Boolean (false)
+//if 'name' is not blank, then '!' will convert to a Boolean (NOT TRUE)
+while (!name) {
+  //'while (name === "")' <-- this also would work for the 'while' line
+  name = prompt("What is your name?");
+}
+
+console.log(name);
